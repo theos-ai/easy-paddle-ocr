6396a560668f04abb090bc9753e5cd0fd4407898
ramacastro <ramicasmar@gmail.com>
2023-02-21 15:43:51 +0100

    Fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def load_readme():
     with open(os.path.join(folder_path, 'README.md')) as f:
         return f.read()
 
-PACKAGE_VERSION = '0.0.2'
+PACKAGE_VERSION = '0.0.3'
 
 setuptools.setup(
     name = 'easy-paddle-ocr',
